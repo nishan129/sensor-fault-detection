a66886e79d149449b177830d56c4f664e07b6747
Nishant Borkar <107128913+nishan129@users.noreply.github.com>
2023-08-27 14:15:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,7 @@
 # if __name__ == '__main__':
 #     training_pipeline_config = TrainingPipelineConfig()
 #     data_ingention_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-#     print(data_ingention_config.__dict__)
-    
+#     print(data_ingention_config.__dict__)    
 #for test trainingpipeline from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
 import os,sys
